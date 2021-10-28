9c396bbe775d83030f9c6fcfecaadca007352cd0
DoanKhiem <73279214+DoanKhiem@users.noreply.github.com>
2021-10-28 15:50:26 +0700

    detail lần 2

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,12 +1,14 @@
 export class Product {
- sku!: string;
- name!: string;
- description!: string;
- unitPrice!: number;
- imageUrl!: string;
- active!: boolean;
- unitsInStock!: number;
- dateCreated!: Date;
- lastUpdate!: Date;
+    id!: string;
+    sku!: string;
+    name!: string;
+    description!: string;
+    unitPrice!: number;
+    imageUrl!: string;
+    active!: boolean;
+    unitsInStock!: number;
+    dateCreated!: Date;
+    lastUpdate!: Date;
+    
 }
 
