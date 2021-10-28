455133f123ade3be7b2e51dc3308a6e9ba81251f
DoanKhiem <73279214+DoanKhiem@users.noreply.github.com>
2021-10-28 15:45:03 +0700

    revert 1 commit

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,14 +1,12 @@
 export class Product {
-    id!: string;
-    sku!: string;
-    name!: string;
-    description!: string;
-    unitPrice!: number;
-    imageUrl!: string;
-    active!: boolean;
-    unitsInStock!: number;
-    dateCreated!: Date;
-    lastUpdate!: Date;
-    
+ sku!: string;
+ name!: string;
+ description!: string;
+ unitPrice!: number;
+ imageUrl!: string;
+ active!: boolean;
+ unitsInStock!: number;
+ dateCreated!: Date;
+ lastUpdate!: Date;
 }
 
